7591487469dc6193d244c786523850c0bd8564d8
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2023-04-28 23:24:32 +0500

    Working on search module

--- src/screens/FriendScreen.js
+++ src/screens/FriendScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from 'react';
 import {
   View,
   TextInput,
@@ -6,24 +6,25 @@ import {
   Text,
   Image,
   Pressable,
-} from "react-native";
-import AntDesign from "react-native-vector-icons/AntDesign";
-import Button from "../components/Button";
-import { API_URL } from "../../API_URL";
+} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Button from '../components/Button';
+import {API_URL} from '../../API_URL';
+import {theme} from '../Theme/Theme';
 
 const FriendScreen = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
   const [data, setData] = useState([]);
 
   const api_call = () => {
     console.log(API_URL);
     fetch(`${API_URL}/api/v1/all_users`, {
-      method: "GET",
+      method: 'GET',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     })
-      .then(async (res) => {
+      .then(async res => {
         try {
           // console.log(res);
           const jsonRes = await res.json();
@@ -38,7 +39,7 @@ const FriendScreen = () => {
           console.log(err);
         }
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   };
@@ -49,19 +50,54 @@ const FriendScreen = () => {
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Search"
-        onChangeText={(newSearchTerm) => setSearchTerm(newSearchTerm)}
-        value={searchTerm}
-      />
+      <View
+        style={{
+          height: '12%',
+          backgroundColor: theme.colors.primary,
+          width: '100%',
+          display: 'flex',
+          alignItems: 'space-between',
+          justifyContent: 'center',
+          paddingHorizontal: 0,
+        }}>
+        <View
+          style={{
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <AntDesign
+            name="search1"
+            color="white"
+            size={32}
+            style={{marginRight: 10}}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Search"
+            onChangeText={newSearchTerm => setSearchTerm(newSearchTerm)}
+            value={searchTerm}
+          />
+          <Pressable
+            style={{
+              padding: 10,
+              backgroundColor: theme.colors.secondary,
+              color: 'white',
+              borderRadius: 10,
+            }}>
+            <Text>Search</Text>
+          </Pressable>
+        </View>
+      </View>
       {/* Friend list section */}
       <View>
         {data?.map((contact, index) => (
           <View key={index} style={styles.row}>
-            <View style={{ display: "flex", flexDirection: "row" }}>
+            <View style={{display: 'flex', flexDirection: 'row'}}>
               <Image
-                source={require("../assets/me.png")}
+                source={require('../assets/me.png')}
                 style={styles.image}
               />
               <Text style={styles.text}>
@@ -81,50 +117,45 @@ const FriendScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    marginTop: 50,
-    display: "flex",
-    flexDirection: "column",
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
   },
   input: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     borderRadius: 10,
     height: 40,
     paddingHorizontal: 10,
-    backgroundColor: "#FAF9F6",
-    width: "90%",
-    padding: 10,
+    backgroundColor: '#FAF9F6',
+    width: '60%',
+    marginRight: 10,
   },
   TextSearch: {
-    color: "#858585",
+    color: '#858585',
     fontSize: 20,
     paddingLeft: 10,
   },
-  // btnStyle: {
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  // },
   row: {
-    display: "flex",
-    flexDirection: "row",
+    display: 'flex',
+    flexDirection: 'row',
     marginTop: 20,
-    alignItems: "center",
+    alignItems: 'center',
     marginLeft: 70,
-    justifyContent: "space-between",
-    width: "80%",
+    justifyContent: 'space-between',
+    width: '80%',
   },
   text: {
-    color: "black",
+    color: 'black',
     fontSize: 18,
     marginLeft: 5,
   },
   starredIcon: {
     width: 55,
     height: 55,
-    backgroundColor: "#333333",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: '#333333',
+    justifyContent: 'center',
+    alignItems: 'center',
     borderRadius: 20,
   },
   image: {
--- src/screens/Streams/Host/HostStreamScreen.js
+++ src/screens/Streams/Host/HostStreamScreen.js
@@ -1,25 +1,32 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet, Pressable} from 'react-native';
 import HostComponent from './Components.js/Host';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import 'react-native-get-random-values';
+import {v4 as uuidv4} from 'uuid';
 
 export default function HostStreamScreem({navigation}) {
   const APP_ID = 845259951;
   const APP_SIGNIN =
     '2301d5bd06367344f2003efba05cbc27457df8be4a583ae7419541bc3f33b95b';
-  const USER_ID = '12345';
+  const USER_ID = uuidv4();
   const USERNAME = 'Muneeb';
-  const LIVE_ID = '12345';
+  const LIVE_ID = uuidv4();
 
   return (
     <>
-      <HostComponent
-        navigation={navigation}
-        APP_ID={APP_ID}
-        APP_SIGNIN={APP_SIGNIN}
-        USERNAME={USERNAME}
-        USER_ID={USER_ID}
-        LIVE_ID={LIVE_ID}
-      />
+      <SafeAreaView>
+        <View>
+          <HostComponent
+            navigation={navigation}
+            APP_ID={APP_ID}
+            APP_SIGNIN={APP_SIGNIN}
+            USERNAME={USERNAME}
+            USER_ID={USER_ID}
+            LIVE_ID={LIVE_ID}
+          />
+        </View>
+      </SafeAreaView>
     </>
   );
 }
