f078a9eb9155aa273395bc8803e02bb540bf0843
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2023-05-14 01:04:28 +0500

    Working on followers and followings

--- src/screens/Profile/Followers.js
+++ src/screens/Profile/Followers.js
@@ -1,175 +1,81 @@
-import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  ScrollView,
-  Button,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import BackButton from "../../components/BackButton";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { theme } from "../../Theme/Theme";
-import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+import React from 'react';
+import {View, Text, StyleSheet, Image, ScrollView, Button} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import BackButton from '../../components/BackButton';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {theme} from '../../Theme/Theme';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+import {useUserDataHandler} from '../../../context/UserInfoContext';
+import ScreenHeader from '../../components/Header/ScreenHeader';
 
-function Followers({ navigation }) {
-  const FollowData = [
-    {
-      key: "1",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Umer",
-      subtitle: "@umery",
-    },
-    {
-      key: "2",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Ali",
-      subtitle: "@ali",
-    },
-    {
-      key: "3",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Haider",
-      subtitle: "@haider",
-    },
-    {
-      key: "4",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Fatima",
-      subtitle: "@fatima",
-    },
-    {
-      key: "5",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Ayesha",
-      subtitle: "@ayesha",
-    },
-    {
-      key: "6",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Fatima",
-      subtitle: "@fatima",
-    },
-    {
-      key: "5",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Ayesha",
-      subtitle: "@ayesha",
-    },
-    {
-      key: "4",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Fatima",
-      subtitle: "@fatima",
-    },
-    {
-      key: "5",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Ayesha",
-      subtitle: "@ayesha",
-    },
-    {
-      key: "4",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Fatima",
-      subtitle: "@fatima",
-    },
-    {
-      key: "5",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Ayesha",
-      subtitle: "@ayesha",
-    },
-    {
-      key: "4",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Fatima",
-      subtitle: "@fatima",
-    },
-    {
-      key: "5",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Ayesha",
-      subtitle: "@ayesha",
-    },
-  ];
+function Followers({navigation}) {
+  //** Global Context */
+  const {userData, followers} = useUserDataHandler();
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            width: "100%",
-            paddingHorizontal: "5%",
-            marginTop: 20,
-          }}
-        >
-          <TouchableOpacity onPress={navigation.goBack}>
-            <Image
-              style={{ width: 30, height: 30 }}
-              source={require("../../assets/arrow_back.png")}
-            />
-          </TouchableOpacity>
-          <Text style={{ color: theme.colors.text, fontSize: 25 }}>
-            Followers (1000+)
-          </Text>
-          <Text></Text>
-        </View>
-        <View
-          style={{
-            width: "100%",
-            height: 2,
-            marginTop: 20,
-            backgroundColor: "#e3e1e3",
-          }}
-        ></View>
-      </View>
-      <ScrollView style={{ flexGrow: 1 }}>
-        <View style={styles.followersList}>
-          {FollowData.map((element, index) => {
-            return (
-              <View key={index}>
-                <View style={styles.data}>
-                  <View style={styles.ImgnText}>
-                    {element.id}
-                    <Image style={styles.dataImage} source={element.photo} />
-                    <Text style={styles.dataText}>{element.title}</Text>
-                  </View>
-                  <Pressable
-                    style={{
-                      justifyContent: "center",
-                      padding: 12,
-                      backgroundColor: theme.colors.secondary,
-                      borderRadius: 10,
-                    }}
-                  >
-                    <Text>Follow</Text>
-                  </Pressable>
-                </View>
-                <View
-                  style={{
-                    width: "100%",
-                    height: 2,
-                    backgroundColor: "#e3e1e3",
-                    marginVertical: 10,
-                  }}
-                ></View>
+    <>
+      <ScreenHeader navigation={navigation} title="Followers" />
+      <SafeAreaView>
+        <View style={styles.container}></View>
+        <ScrollView style={{flexGrow: 1}}>
+          <View style={styles.followersList}>
+            {followers.length == 0 ? (
+              <View
+                style={{
+                  width: '100%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Text style={{fontSize: 22}}>No followers!</Text>
               </View>
-            );
-          })}
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+            ) : (
+              <></>
+            )}
+            {followers.map((element, index) => {
+              return (
+                <View key={index}>
+                  <View style={styles.data}>
+                    <View style={styles.ImgnText}>
+                      {/* {element.user.id} */}
+                      <Image
+                        style={styles.dataImage}
+                        source={require('../../assets/prof.jpeg')}
+                      />
+                      <Text style={styles.dataText}>
+                        {element.user.user_name}
+                      </Text>
+                    </View>
+                    <Pressable
+                      style={{
+                        justifyContent: 'center',
+                        padding: 12,
+                        backgroundColor: theme.colors.secondary,
+                        borderRadius: 10,
+                      }}>
+                      <Text>Follow</Text>
+                    </Pressable>
+                  </View>
+                  <View
+                    style={{
+                      width: '100%',
+                      height: 2,
+                      backgroundColor: '#e3e1e3',
+                      marginVertical: 10,
+                    }}></View>
+                </View>
+              );
+            })}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
-    height: "10%",
+    alignItems: 'center',
+    height: '10%',
   },
   followersList: {
     padding: 10,
@@ -177,18 +83,18 @@ const styles = StyleSheet.create({
 
   data: {
     margin: 5,
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     gap: 10,
   },
   ImgnText: {
-    display: "flex",
-    flexDirection: "row",
+    display: 'flex',
+    flexDirection: 'row',
     // justifyContent: "space-evenly",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   dataImage: {
     width: 55,
--- src/screens/Profile/Following.js
+++ src/screens/Profile/Following.js
@@ -1,167 +1,79 @@
-import React from "react";
-import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { theme } from "../../Theme/Theme";
-import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+import React from 'react';
+import {View, Text, StyleSheet, Image, ScrollView} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {theme} from '../../Theme/Theme';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+import {useUserDataHandler} from '../../../context/UserInfoContext';
+import ScreenHeader from '../../components/Header/ScreenHeader';
 
-function Following({ navigation }) {
-  const FollowData = [
-    {
-      key: "1",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Umer",
-      subtitle: "@umery",
-    },
-    {
-      key: "2",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Ali",
-      subtitle: "@ali",
-    },
-    {
-      key: "3",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Haider",
-      subtitle: "@haider",
-    },
-    {
-      key: "4",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Fatima",
-      subtitle: "@fatima",
-    },
-    {
-      key: "5",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Ayesha",
-      subtitle: "@ayesha",
-    },
-    {
-      key: "6",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Fatima",
-      subtitle: "@fatima",
-    },
-    {
-      key: "5",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Ayesha",
-      subtitle: "@ayesha",
-    },
-    {
-      key: "4",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Fatima",
-      subtitle: "@fatima",
-    },
-    {
-      key: "5",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Ayesha",
-      subtitle: "@ayesha",
-    },
-    {
-      key: "4",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Fatima",
-      subtitle: "@fatima",
-    },
-    {
-      key: "5",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Ayesha",
-      subtitle: "@ayesha",
-    },
-    {
-      key: "4",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Fatima",
-      subtitle: "@fatima",
-    },
-    {
-      key: "5",
-      photo: require("../../assets/prof.jpeg"),
-      title: "Ayesha",
-      subtitle: "@ayesha",
-    },
-  ];
+function Following({navigation}) {
+  //** Global Context */
+  const {userData, followings} = useUserDataHandler();
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            width: "100%",
-            paddingHorizontal: "5%",
-            marginTop: 20,
-          }}
-        >
-          <TouchableOpacity onPress={navigation.goBack}>
-            <Image
-              style={{ width: 30, height: 30 }}
-              source={require("../../assets/arrow_back.png")}
-            />
-          </TouchableOpacity>
-          <Text style={{ color: theme.colors.text, fontSize: 25 }}>
-            Followings (303)
-          </Text>
-          <Text></Text>
+    <>
+      <ScreenHeader navigation={navigation} title="Followings" />
+      <SafeAreaView>
+        <View style={styles.container}>
+          <View
+            style={{
+              width: '100%',
+              height: 2,
+              marginTop: 20,
+              backgroundColor: '#e3e1e3',
+            }}></View>
         </View>
-        <View
-          style={{
-            width: "100%",
-            height: 2,
-            marginTop: 20,
-            backgroundColor: "#e3e1e3",
-          }}
-        ></View>
-      </View>
-      <ScrollView style={{ flexGrow: 1 }}>
-        <View style={styles.followersList}>
-          {FollowData.map((element, index) => {
-            return (
-              <View key={index}>
-                <View style={styles.data}>
-                  <View style={styles.ImgnText}>
-                    {element.id}
-                    <Image style={styles.dataImage} source={element.photo} />
-                    <Text style={styles.dataText}>{element.title}</Text>
-                  </View>
-                  <Pressable
-                    style={{
-                      justifyContent: "center",
-                      padding: 12,
-                      backgroundColor: theme.colors.secondary,
-                      borderRadius: 10,
-                    }}
-                  >
-                    <Text>Follow</Text>
-                  </Pressable>
-                </View>
-                <View
-                  style={{
-                    width: "100%",
-                    height: 2,
-                    backgroundColor: "#e3e1e3",
-                    marginVertical: 10,
-                  }}
-                ></View>
+        <ScrollView style={{flexGrow: 1}}>
+          <View style={styles.followersList}>
+            {followings.length == 0 ? (
+              <View
+                style={{
+                  width: '100%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Text style={{fontSize: 22}}>No Followings!</Text>
               </View>
-            );
-          })}
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+            ) : (
+              <></>
+            )}
+            {followings.map((element, index) => {
+              return (
+                <View key={index}>
+                  <View style={styles.data}>
+                    <View style={styles.ImgnText}>
+                      {/* {element.user.id} */}
+                      <Image
+                        style={styles.dataImage}
+                        source={require('../../assets/prof.jpeg')}
+                      />
+                      <Text style={styles.dataText}>
+                        {element.user.user_name}
+                      </Text>
+                    </View>
+                  </View>
+                  <View
+                    style={{
+                      width: '100%',
+                      height: 2,
+                      backgroundColor: '#e3e1e3',
+                      marginVertical: 10,
+                    }}></View>
+                </View>
+              );
+            })}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
-    height: "10%",
+    alignItems: 'center',
+    height: '10%',
   },
   followersList: {
     padding: 10,
@@ -169,18 +81,18 @@ const styles = StyleSheet.create({
 
   data: {
     margin: 5,
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     gap: 10,
   },
   ImgnText: {
-    display: "flex",
-    flexDirection: "row",
+    display: 'flex',
+    flexDirection: 'row',
     // justifyContent: "space-evenly",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   dataImage: {
     width: 55,
