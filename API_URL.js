487fd037dc062ebdb673567b9ad7ff64072f4fde
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2023-06-02 02:42:39 +0500

    chore: changes production url

--- API_URL.js
+++ API_URL.js
@@ -1,7 +1,7 @@
 // const uri = 'http://192.168.255.227:4001';
 // dev url
 
-const uri = 'http://13.127.232.204:4001';
+const uri = 'http://projectorate.org:4001';
 // production url
 
 export const API_URL = uri;
